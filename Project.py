4abe12b2e6d1f96e34ce6e4337de375a9581e242
Maverick9062 <59537068+Maverick9062@users.noreply.github.com>
2021-05-17 21:58:41 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,36 @@
+# Creating an application
+# there is a folder name ... new folder1...where audios,videos,images are there
+# so you have to keep it in separate folders 
+import os, shutil
+
+# NOTE --> you can write every single extension inside tuples(becuz we don't want to change the values)
+dict_extensions = {
+    'audio_extensions' : ('.mp3','.m4a','.wav','.flac'),
+    'video_extensions' : ('.mp4','.mkv','.MKV','.flv','.mpeg','.MOV'),
+    'documents_extensions' : ('.docx','.pdf','.txt'),
+}
+
+folderpath = input('enter folder path : ')
+
+def file_finder(folder_path,file_extensions):
+    files = []
+    for file in os.listdir(folder_path):
+        for extension in file_extensions:
+            if file.endswith(extension):
+                files.append(file)
+    return files
+
+#print(file_finder(folderpath,video_extensions))
+
+for extension_type,extension_tuple in dict_extensions.items():
+    #print('calling file finder')
+    #print(file_finder(folderpath,extension_tuple))
+    folder_name = extension_type.split('_')[0] + 'Files'
+    folder_path = os.path.join(folderpath,folder_name)
+    os.mkdir(folder_path)
+    for item in file_finder(folderpath,extension_tuple):
+        item_path = os.path.join(folderpath,item)
+        item_new_path = os.path.join(folder_path,item)
+        shutil.move(item_path,item_new_path)
+
+        
